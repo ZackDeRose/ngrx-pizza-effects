f0f8a47acb7cf74bbd3f751ab5355c30248657fd
Zack DeRose <zack@nrwl.io>
2021-02-03 03:55:54 -0700

    adding ngrx component package

--- apps/pizza/src/app/app.module.ts
+++ apps/pizza/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { PizzaComponent } from './pizza/pizza.component';
 import { StoreModule } from '@ngrx/store';
 import { reducer, PIZZA_FEATURE_NAME } from './pizza.store';
 import { ReactiveComponentModule } from '@ngrx/component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, PizzaComponent],
+  declarations: [AppComponent, PizzaComponent, HomeComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({ [PIZZA_FEATURE_NAME]: reducer }),
     ReactiveComponentModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'order', component: PizzaComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
